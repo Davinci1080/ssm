4a7bd9444dc437a7bbbbb55176fd0d083c084b31
DaVinci <zaanahd@qq.com>
2019-11-27 10:17:32 +0800

    添加了Helloword方法111

--- ssmcode003/src/com/itcast/testtt/Hello.java
+++ ssmcode003/src/com/itcast/testtt/Hello.java
@@ -2,6 +2,6 @@ package com.itcast.testtt;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("fgdsfgsdfghello world");
     }
 }
